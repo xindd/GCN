c1935cf062de433143f700924b4eea5ad3340eb1
xindd <53898924+xindd@users.noreply.github.com>
2020-10-10 08:26:36 +0800

    Add files via upload

---
+++ UseNet.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# @Author: XinWang
+# @Time:2019/7/26 17:37
+# @FileName: UseNet.py
+# @Software: PyCharm
+from models import *
+from loadNetParams import *
+
+dataset = LoadNetParams()
+net2, feature2 = train_model(dataset)
+
+net2.load_parameters('data/net_params')
+x = nd.random.normal(0,1,shape=(len(dataset.genelist_order), 1))
+print(net2(x))
